9dfe06d6d4390c0839b699a454e72b2fd8813678
bksindhu <86909876+bksindhu@users.noreply.github.com>
2021-12-18 14:26:44 +0530

    Add files via upload
    Updated files against Wk14 assignment

--- test_app.py
+++ test_app.py
@@ -24,3 +24,33 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+# test to check if Iris Setosa is classified correctly
+def test_pred_Setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa"}
+
+# test to check if Iris Versicolor is classified correctly
+def test_pred_Versicolor():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 6.5,
+        "sepal_width": 2.8,
+        "petal_length": 4.6,
+        "petal_width": 1.5,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour"}
